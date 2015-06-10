6ef0a37bc3f749f001772962b79fe4fa5e429341
hsinhuang <leon.hxin@gmail.com>
2015-06-10 23:45:21 +0800

    add cache things

--- util.py
+++ util.py
@@ -2,10 +2,36 @@
 # -*- coding:utf-8 -*-
 
 
+import os
 import pandas as pd
 import numpy as np
+import pickle as pkl
 
 
+def dump(obj, path):
+    with open(path, 'wb') as f:
+        pkl.dump(obj, f)
+
+
+def fetch(path):
+    with open(path, 'rb') as f:
+        data = pkl.load(f)
+    return data
+
+
+def __cache__(func):
+    def cached_func(path):
+        pkl_path = path + '.pkl'
+        if os.path.exists(pkl_path):
+            data = fetch(pkl_path)
+        else:
+            data = func(path)
+            dump(data, pkl_path)
+        return data
+    return cached_func
+
+
+@__cache__
 def load_log(path):
     """Load log set as pandas DataFrame"""
     log_set = pd.read_csv(path, parse_dates=['time'])
@@ -13,16 +39,19 @@ def load_log(path):
     return log_set
 
 
+@__cache__
 def load_enrollment(path):
     """Load enrollment set as pandas DataFrame"""
     return pd.read_csv(path)
 
 
+@__cache__
 def load_object(path):
     """Load object set as pandas DataFrame"""
     return pd.read_csv(path, parse_dates=['start'])
 
 
+@__cache__
 def load_val_y(path):
     """Load labels of validation set as numpy ndarray"""
     return np.loadtxt(path, dtype=np.int, delimiter=',')
