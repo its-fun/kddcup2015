087dc5a633239f1e8e70c4cc2a04a3b5ec692096
hsinhuang <leon.hxin@gmail.com>
2015-06-14 16:57:37 +0800

    add utilities

--- util.py
+++ util.py
@@ -47,21 +47,52 @@ def load_log(path):
     return log_set
 
 
+def load_log_train():
+    """Load training log set as pandas DataFrame"""
+    return load_log(config.TRAIN_DATASET_PATHS['log'])
+
+
+def load_log_test():
+    """Load testing log set as pandas DataFrame"""
+    return load_log(config.TEST_DATASET_PATHS['log'])
+
+
+def load_logs():
+    """Load all log sets as pandas DataFrame"""
+    return load_log_train().append(load_log_test(), ignore_index=True)
+
+
 @__cache__
 def load_enrollment(path):
     """Load enrollment set as pandas DataFrame"""
     return pd.read_csv(path)
 
 
+def load_enrollment_train():
+    """Load training enrollment set as pandas DataFrame"""
+    return load_enrollment(config.TRAIN_DATASET_PATHS['enrollment'])
+
+
+def load_enrollment_test():
+    """Load testing enrollment set as pandas DataFrame"""
+    return load_enrollment(config.TEST_DATASET_PATHS['enrollment'])
+
+
+def load_enrollments():
+    """Load all enrollment sets as pandas DataFrame"""
+    return load_enrollment_train().append(load_enrollment_test(),
+                                          ignore_index=True)
+
+
 @__cache__
-def load_object(path):
+def load_object(path=config.OBJECT_PATH):
     """Load object set as pandas DataFrame"""
     return pd.read_csv(path, parse_dates=['start'], na_values=['null'])
 
 
 @__cache__
-def load_val_y(path):
-    """Load labels of validation set as numpy ndarray"""
+def load_val_y(path=config.TRAIN_DATASET_PATHS['truth']):
+    """Load enrollment-labels pairs of validation set as numpy ndarray"""
     return np.loadtxt(path, dtype=np.int, delimiter=',')
 
 
